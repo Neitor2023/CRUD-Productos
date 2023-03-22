971bb2fac918ddf396aa607c7def83968baf48ad
Romy Garcia <neitorme@gmail.com>
2023-03-21 19:39:08 -0500

    Probando menu bomba creciente

--- src/assets/components/ConfDelete.jsx
+++ src/assets/components/ConfDelete.jsx
@@ -4,7 +4,7 @@ const ConfDelete = ({ prodDele, cancelDele, deleProduct }) => {
     return (
         <div className='container'>
             <div className='dele'>
-                <h3>DESEAS ELIMINAR {prodDele.name} ?</h3>
+                <h3>DESEAS ELIMINAR <br /> {prodDele.name} ?</h3>
                 <h4>Esta operación es irreversible</h4>
                 <button className="btn_delet" onClick={() => deleProduct(prodDele.id, false)} >Eliminar</button>
                 <button className="btn_close" onClick={() => cancelDele(false)} ><strong className='x'>X</strong></button>
